ba927113f7c633b5a036a6053048e208db2ca00c
Michael Duncan <mwduncan2018@gmail.com>
2025-09-23 16:43:35 -0400

    Add test models

---
+++ cypress/support/models/index.ts
@@ -0,0 +1,2 @@
+export * from './product.model';
+export * from './watch-list-entry.model';
---
+++ cypress/support/models/watch-list-entry.model.ts
@@ -0,0 +1,9 @@
+export class WatchListEntry {
+  manufacturer: string;
+  model: string;
+
+  constructor(manufacturer: string, model: string) {
+    this.manufacturer = manufacturer;
+    this.model = model;
+  }
+}
