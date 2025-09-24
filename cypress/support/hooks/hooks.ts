2ec40e9947eb2850d4b4dd48e9b1d890c1626b56
Michael Duncan <mwduncan2018@gmail.com>
2025-09-24 15:48:49 -0400

    Updates

--- cypress/support/hooks/hooks.ts
+++ cypress/support/hooks/hooks.ts
@@ -1,12 +1,8 @@
 import { Before, After } from '@badeball/cypress-cucumber-preprocessor';
-import { Product } from '../models/product.model';
 
 Before(() => {
   cy.clearCookies();
   cy.clearLocalStorage();
-  cy.fixture<Product[]>('product-test-data.json').then((productTestData) => {
-    cy.wrap(productTestData).as('productTestData')
-  })
   cy.wrap({}).as('context')
 
   cy.visit('/ProductList')
